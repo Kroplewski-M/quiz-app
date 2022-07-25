acae7683fd2d112bdf1812841393870d47e38d15
Mateusz <kroplewskimateusz@gmail.com>
2022-07-25 17:56:24 +0100

    small ui changes

--- src/components/results-template.vue
+++ src/components/results-template.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-[100%] h-[200px] bg-purple-400 rounded-md">
+  <div class="w-[100%] h-[200px] bg-purple-400 rounded-md border-2 border-solid border-gray-100">
     <p class="text-[50px] font-bold text-gray-50 pt-10">You got sample results 1!</p>
     <p class="pt-10 ">Small desc of results</p>
   </div>
