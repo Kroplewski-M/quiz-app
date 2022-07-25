98924e7cc91973afdd3847982948e054dec889b3
Mateusz <kroplewskimateusz@gmail.com>
2022-07-25 22:05:59 +0100

    fixed question not showing bug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="w-[100vw] h-[100vh] bg-blue-500">
+<div class="w-[100vw] h-[100vh] bg-blue-500" @click="getQ()">
   <div class=" mx-auto w-[700px] h-[auto] bg-blue-500 rounded-md text-center pt-10">
-    <questionTemplate v-if="questionsAnswered < amountofQuestions"></questionTemplate>
+    <questionTemplate :questions="questions"  v-if="questionsAnswered < amountofQuestions"></questionTemplate>
     <ResultsTemplate v-else></ResultsTemplate>
  </div>
  <div class="mx-auto w-[100px]">
@@ -26,18 +26,20 @@ export default {
 },
   data(){
     return{
-      questions: [],
-      questionsAnswered: 4,
+      questions: quiz,
+      questionsAnswered: 0,
       amountofQuestions: 4,
       correctAnswers: 0,
     }
   },
   methods:{
+    getQ(){
+      console.log("click");
+      console.log(this.questions);
+    }
    },
-  mounted(){
-   this.questions = quiz;
-  // console.log(this.questions.question[0]);
-   console.log(this.questions.questions[0].question);
+  mount(){
+  
   },
 
 }
--- src/components/Questions-template.vue
+++ src/components/Questions-template.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="absolute w-[100%] max-w-[700px] h-[70px] bg-red-400 rounded-md"></div>
     <p class="z-50 relative text-[20px] pt-[20px] font-bold font-mono">1 out of 4 questions answered</p>
-    <div class="w-[100%] h-[100px] bg-green-500 border-b-2 border-black">
-      <p class="pt-[47px]">Template Question</p>
+    <div class="w-[100%] h-[100px] bg-green-500 border-b-2 border-black border-solid ">
+      <p class="pt-[47px]">{{questions.questions[0].question}}</p>
     </div>
       <div class="w-[100%] h-[100%] bg-gray-200 rounded-b-md">
       <div>
@@ -16,7 +16,15 @@
 
 <script>
 export default {
-  
+  props: ["questions"],
+  data(){
+    return{
+    }
+  },
+  methods:{
+
+  }
+
 }
 </script>
 <style>
