17ab1d6c4c8776e45b1c54ee1f1867111b2050e9
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-07-25 14:13:35 +0100

    segregated answers and questions components - added anims to answer cards

--- src/components/Questions-template.vue
+++ src/components/Questions-template.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1 class="hidden">Question</h1>
+    <div class="absolute w-[100%] max-w-[700px] h-[70px] bg-red-400 rounded-md"></div>
+    <p class="z-50 relative text-[20px] pt-[20px] font-bold font-mono">1 out of 4 questions answered</p>
+    <div class="w-[100%] h-[100px] bg-green-500 border-b-2 border-black">
+      <p class="pt-[47px] ">Sample question 1</p>
+    </div>
 </template>
 
 <script>
