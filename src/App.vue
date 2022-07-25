8fb2ab46da291bafac4ed33900c371144699edd2
Mateusz <kroplewskimateusz@gmail.com>
2022-07-25 22:23:09 +0100

    added more props to questions and v-if to reset button

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="w-[100vw] h-[100vh] bg-blue-500" @click="getQ()">
   <div class=" mx-auto w-[700px] h-[auto] bg-blue-500 rounded-md text-center pt-10">
-    <questionTemplate :questions="questions"  v-if="questionsAnswered < amountofQuestions"></questionTemplate>
+    <questionTemplate :questions="questions" :questionsAnswered="questionsAnswered"  v-if="questionsAnswered < amountofQuestions"></questionTemplate>
     <ResultsTemplate v-else></ResultsTemplate>
  </div>
  <div class="mx-auto w-[100px]">
-  <button class="w-[100px] h-[40px] bg-red-400 font-semibold mt-[30px] active:mt-[33px]">Reset</button>
+  <button v-if="questionsAnswered == amountofQuestions" class="w-[100px] h-[40px] bg-red-400 font-semibold mt-[30px] active:mt-[33px]">Reset</button>
 </div>
 </div>
 </template>
@@ -27,7 +27,7 @@ export default {
   data(){
     return{
       questions: quiz,
-      questionsAnswered: 0,
+      questionsAnswered: 1,
       amountofQuestions: 4,
       correctAnswers: 0,
     }
