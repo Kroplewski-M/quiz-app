7c0d9676a48c15ad9e00b70105744523b2b6e54b
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-07-26 10:34:14 +0100

    updated results reply to your score

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <div class="w-[100vw] h-[100vh] bg-blue-500">
   <div class=" mx-auto w-[700px] h-[auto] bg-blue-500 rounded-md text-center pt-10">
     <questionTemplate @question-answered="questionAnswered" :questions="questions" :questionsAnswered="questionsAnswered"  v-if="questionsAnswered < amountofQuestions"></questionTemplate>
-    <ResultsTemplate v-else></ResultsTemplate>
+    <ResultsTemplate :correctAnswers="correctAnswers" v-else></ResultsTemplate>
  </div>
  <div class="mx-auto w-[100px]">
   <button v-if="questionsAnswered == amountofQuestions" @click="reset()" class="w-[100px] h-[40px] bg-red-400 font-semibold mt-[30px] active:mt-[33px]">Reset</button>
--- src/components/results-template.vue
+++ src/components/results-template.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="w-[100%] h-[200px] bg-purple-400 rounded-md border-2 border-solid border-gray-100">
-    <p class="text-[50px] font-bold text-gray-50 pt-10">You got sample results 1!</p>
-    <p class="pt-10 ">Small desc of results</p>
+    <p class="text-[50px] font-bold text-gray-50 pt-10 drop-shadow-xl">You got {{this.correctAnswers}} out of 4 Correct!</p>
+    <p class="pt-[25px] font-semibold">{{getResponse()}}</p>
   </div>
 </template>
 
 <script>
 export default {
+  props:['correctAnswers',],
 
+data(){
+  return{
+    response:"",
+  }
+},
+methods:{
+  getResponse(){
+    if(this.correctAnswers <= 2){
+      this.response = "You need to study harder!"
+      return this.response;
+    }
+    else{
+      this.response = "Congrats you know some stuff!"
+      return this.response;
+    }
+  }
+}
 }
 </script>
 
