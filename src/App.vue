7ddc186f0d16594bf94d828eba84d4df90f9d45b
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-07-27 12:37:58 +0100

    added  welcome content before the quiz

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
 <div class="w-[100vw] h-[100vh] bg-blue-500">
+  <WelcomeTemplate v-if="welcome"></WelcomeTemplate>
   <div class=" mx-auto w-[400px] md:w-[700px] h-[auto] bg-blue-500 rounded-md text-center pt-10">
-    <questionTemplate @question-answered="questionAnswered" :questions="questions" :questionsAnswered="questionsAnswered"  v-if="questionsAnswered < amountofQuestions"></questionTemplate>
-    <ResultsTemplate :correctAnswers="correctAnswers" v-else></ResultsTemplate>
+    <questionTemplate @question-answered="questionAnswered" :questions="questions" :questionsAnswered="questionsAnswered" v-if="questionsAnswered < amountofQuestions && welcome == false"></questionTemplate>
+    <ResultsTemplate :correctAnswers="correctAnswers" v-else-if="questionsAnswered >= amountofQuestions && welcome == false"></ResultsTemplate>
  </div>
  <div class="mx-auto w-[100px]">
   <button v-if="questionsAnswered == amountofQuestions" @click="reset()" class="w-[100px] h-[40px] bg-red-400 font-semibold mt-[30px] active:mt-[33px]">Reset</button>
@@ -15,7 +16,7 @@ import '../public/index.css';
 import questionTemplate from './components/Questions-template.vue';
 import quiz from '../questions.json';
 import ResultsTemplate from './components/results-template.vue';
-
+import WelcomeTemplate from './components/Welcome-template.vue';
 
 
 
@@ -24,6 +25,7 @@ export default {
   components: {
     questionTemplate,
     ResultsTemplate,
+    WelcomeTemplate,
 },
   data(){
     return{
@@ -31,12 +33,14 @@ export default {
       questionsAnswered: 0,
       amountofQuestions: 4,
       correctAnswers: 0,
+      welcome : true,
     }
   },
   methods:{
     reset(){
       this.questionsAnswered = 0;
       this.correctAnswers = 0;
+      this.welcome = true;
     },
     questionAnswered(isCorrect){
       if(isCorrect){
